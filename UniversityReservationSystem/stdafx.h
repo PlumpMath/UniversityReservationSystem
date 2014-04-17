d08760fa264980a0506e8b5485cc2e0bfbb10bef
Szymon - Desktop <szymbar099@student.polsl.pl>
2014-04-17 19:01:37 +0200

    added missing stdafx.h

---
+++ UniversityReservationSystem/stdafx.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <stdlib.h>
+#include <crtdbg.h>
+#include <vector>
+
+#include "RoomFactory.h"
+#include "Reservation.h"
+
+#include "GroupController.h"
+#include "StudentController.h"
+#include "TeacherController.h"
+#include "RoomController.h"
+#include "ReservationController.h"
