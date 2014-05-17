4f1f3d7017884231a1464323bca157b430c8dab7
Szymon Bartnik <szymbar099@student.polsl.pl>
2014-05-17 02:25:33 +0200

    Another changes in reservations view model

--- UniversityReservationSystem.Interface/ViewModels/ReservationsVM.cs
+++ UniversityReservationSystem.Interface/ViewModels/ReservationsVM.cs
@@ -1,15 +1,94 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight;
 using UniversityReservationSystem.Interface.Models;
 
 namespace UniversityReservationSystem.Interface.ViewModels
 {
-    public class ReservationsVM : ViewModelBase
+    public class ReservationsVM : IViewModel<Reservation>
     {
+        private string _name;
+        private Group _selectedGroup;
+        private IRoom _selectedRoom;
+        private Teacher _selectedTeacher;
+        private DateTime? _dateOfStart;
+        private DateTime? _dateOfEnd;
         public ObservableCollection<Group> Groups { get; set; }
         public ObservableCollection<IRoom> Rooms { get; set; }
         public ObservableCollection<Teacher> Teachers { get; set; }
         public ObservableCollection<Reservation> Reservations { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    RaisePropertyChanged("Name");
+                }
+            }
+        }
+        public Group SelectedGroup
+        {
+            get { return _selectedGroup; }
+            set
+            {
+                if (_selectedGroup != value)
+                {
+                    _selectedGroup = value;
+                    RaisePropertyChanged("SelectedGroup");
+                }
+            }
+        }
+        public IRoom SelectedRoom
+        {
+            get { return _selectedRoom; }
+            set
+            {
+                if (_selectedRoom != value)
+                {
+                    _selectedRoom = value;
+                    RaisePropertyChanged("SelectedRoom");
+                }
+            }
+        }
+        public Teacher SelectedTeacher
+        {
+            get { return _selectedTeacher; }
+            set
+            {
+                if (_selectedTeacher != value)
+                {
+                    _selectedTeacher = value;
+                    RaisePropertyChanged("SelectedTeacher");
+                }
+            }
+        }
+        public DateTime? DateOfStart
+        {
+            get { return _dateOfStart; }
+            set
+            {
+                if (value != _dateOfStart)
+                {
+                    _dateOfStart = value;
+                    RaisePropertyChanged("DateOfStart");
+                }
+            }
+        }
+        public DateTime? DateOfEnd
+        {
+            get { return _dateOfEnd; }
+            set
+            {
+                if (value != _dateOfEnd)
+                {
+                    _dateOfEnd = value;
+                    RaisePropertyChanged("DateOfEnd");
+                }
+            }
+        }
 
         public ReservationsVM()
         {
@@ -19,6 +98,43 @@ namespace UniversityReservationSystem.Interface.ViewModels
             Rooms = App.Rooms;
             Teachers = App.Teachers;
             Reservations = App.Reservations;
+        }
+
+        protected override void Add()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void Delete()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void UpdateAfterSelection(bool isNull)
+        {
+            if (!isNull)
+            {
+                Name = SelectedItem.Name;
+                DateOfStart = SelectedItem.DateOfStart;
+                DateOfEnd = SelectedItem.DateOfEnd;
+                SelectedGroup = SelectedItem.Group;
+                SelectedRoom = SelectedItem.Room;
+                SelectedTeacher = SelectedItem.Teacher;
+            }
+            else
+            {
+                Name = String.Empty;
+                DateOfStart = null;
+                DateOfEnd = null;
+                SelectedGroup = null;
+                SelectedRoom = null;
+                SelectedTeacher = null;
+            }
         }
     }
 }
